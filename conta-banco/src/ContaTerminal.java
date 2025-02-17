6eba68eed80969d0c5bdab50cdf7f5f233adb006
mateusribeiro08 <mateusribeiroviana0@gmail.com>
2025-02-17 19:40:36 -0300

    finalizando desafio contaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite o número da sua conta: ");
+    int numero = scanner.nextInt();
+    System.out.println("Digite o número da sua agência: ");
+    String agencia = scanner.next();
+    System.out.println("Digite o seu saldo: ");
+    double saldo = scanner.nextDouble();
+    System.out.println("Digite o seu nome: ");
+    String nome = scanner.next();
+
+
+    String mensagem = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %f já está disponível para saque",nome, agencia, numero, saldo);
+    
+
+    System.out.println(mensagem);
     }
 }
